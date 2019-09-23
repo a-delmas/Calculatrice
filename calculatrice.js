8e2b66dcc8c2603979fe214d717a76c8a98eb386
tomsirat <sirat.tom@outlook.com>
2019-09-23 16:58:40 +0200

    start js

---
+++ calculatrice.js
@@ -0,0 +1,7 @@
+var resultElement = document.createElement("div");
+resultElement.id = "result"
+document.body.appendChild(resultElement);
+
+var btnElement = document.createElement("button");
+btnElement.className = "button"
+document.body.appendChild(btnElement);
