465eb91e437356a89cd0c95af8c85fbbd39cb10f
tomsirat <sirat.tom@outlook.com>
2019-09-24 10:30:12 +0200

    go go

--- calculatrice.js
+++ calculatrice.js
@@ -1,7 +1,22 @@
+// Barre résultat
+
+var text = "";
 var resultElement = document.createElement("div");
 resultElement.id = "result"
 document.body.appendChild(resultElement);
 
-var btnElement = document.createElement("button");
-btnElement.className = "button"
-document.body.appendChild(btnElement);
+// Boucle boutons
+
+var btnlist = ["1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "*", "0", "=", "Reset", "/" ];
+
+for (i = 0; i < btnlist.length; i++ ){
+    var button = document.createElement("button");
+    button.innerHTML = btnlist[i];
+    document.body.appendChild(button);
+    button.addEventListener("click", function() {
+        resultElement.innerHTML = this.innerHTML;
+    });
+}
+
+
+
