7144ccc63b856b41128d656fb51b92acc7e51462
a-delmas <aurore.delmas@hotmail.fr>
2019-09-24 12:27:14 +0200

    Modifs quasi finales

--- calculatrice.js
+++ calculatrice.js
@@ -7,16 +7,28 @@ document.body.appendChild(resultElement);
 
 // Boucle boutons
 
-var btnlist = ["1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "*", "0", "=", "Reset", "/" ];
+var btnlist = ["1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "*", "0", "=", "Reset", "/", "." ];
 
 for (i = 0; i < btnlist.length; i++ ){
     var button = document.createElement("button");
     button.innerHTML = btnlist[i];
     document.body.appendChild(button);
     button.addEventListener("click", function() {
-        resultElement.innerHTML = this.innerHTML;
+        var c = this.innerHTML
+        if (c == "=") {
+            resultElement.innerHTML = eval(resultElement.innerHTML);
+        }
+        else {
+            resultElement.innerHTML = resultElement.innerHTML + c;
+        }
+        var d = this.innerHTML
+        if (d == "Reset") {
+            result.innerHTML = "";
+        }
     });
 }
+
+
 
 
 
